b75ae254173b368e4bf550a577b9779e6c1ef486
Alonzo <alondiff@gmail.com>
2020-08-23 17:51:49 +0300

    refactor js script

--- node-sequence.js
+++ node-sequence.js
@@ -1,3 +1,5 @@
+// Andela Javascript Interview
+//
 var input=['a1','a2','a3','a4','a5','a6','a7','a8']
 var add=4
 var minus=3
@@ -5,7 +7,7 @@ output=[]
 
 // Iterate input list performing computation
 // output = ['a1', 'a5', 'a2', 'a6', 'a3',
-//					 'a7', 'a4', 'a8']
+// 'a7', 'a4', 'a8']
 input.map(function(item,index){
 	if(index==0){
 		 output.push(item)
