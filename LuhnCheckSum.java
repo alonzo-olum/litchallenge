097b35dab9229f1aab787fc4880ddf96eb31e1d8
Alonzo <alondiff@gmail.com>
2025-08-19 17:50:09 +0300

    update: fix solutio according to spec

--- LuhnCheckSum.java
+++ LuhnCheckSum.java
@@ -1,36 +1,70 @@
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+/*
+ * LuhnCheckSum a widely used system for validation of
+ * identification number. Using the original number we
+ * double every other digit from the rightmost end.
+ * Example: 1 7 6 2 4 8 3
+ * we double 8, 2, 7 adding each individual digit
+ * otherwise we add the other digits in other positions.
+ * we then check if the total sum is divisible by 10.
+ */
 public class LuhnCheckSum {
-	private static int doubleDigit(int digit) {
+	private static int add(int num) {
 		int checkSum = 0;
-		int doubleDigit = 2 * digit;
-		if (doubleDigit > 10) {
-			checkSum += 1 + doubleDigit % 10;
-		} else {
-			checkSum += doubleDigit;
+		while (num != 0) {
+			int pop = num % 10;
+			num /= 10;
+			checkSum += pop;
 		}
+		System.out.println("add: " + checkSum);
 		return checkSum;
 	}
 
-	private static int evenLengthChecksum(int[] digits) {
-		return IntStream.range(1, digits.length)
+	// oddLengthChecksum calculates the checksum of odd digits
+	private static int oddLengthChecksum(int[] digits) {
+		// for even positions we know we can omit the last digit
+		int evenPositions = IntStream.range(1, digits.length)
 			.filter(i -> i % 2 == 0)
-			.map(i -> doubleDigit(digits[i] - '0'))
+			.map(i -> add(2 * digits[i-1]))
 			.sum();
+		System.out.println("evenPositions for odd-length checksum: " + evenPositions);
+		// +1 in odd positions indicates the last digit is inclusive
+		int oddPositions = IntStream.range(1, digits.length+1)
+			.filter(i -> i % 2 != 0)
+			.map(i -> add(digits[i-1]))
+			.sum();
+		System.out.println("oddPositions for odd-length checksum: " + oddPositions);
+		return oddPositions + evenPositions;
 	}
 
-	private static int oddLengthChecksum(int[] digits) {
-		return IntStream.range(0, digits.length)
+	// evenLengthChecksum calculates the checksum of even digits
+	private static int evenLengthChecksum(int[] digits) {
+		// for odd positions we know we can omit the last digit
+		int oddPositions = IntStream.range(1, digits.length)
 			.filter(i -> i % 2 != 0)
-			.map(i -> digits[i])
+			.map(i -> add(2 * digits[i-1]))
 			.sum();
+		System.out.println("oddPositions for even-length checksum: " + oddPositions);
+		// +1 in even positions indicates the last digit is inclusive
+		int evenPositions = IntStream.range(1, digits.length+1)
+			.filter(i -> i % 2 == 0)
+			.map(i -> add(digits[i-1]))
+			.sum();
+		System.out.println("evenPositions for even-length checksum: " + evenPositions);
+		return oddPositions + evenPositions;
 	}
 
 	private static void calculateChecksum(int[] digits) {
-		int checkSum = evenLengthChecksum(digits) + 
-			oddLengthChecksum(digits);
-		 System.out.println(checkSum % 10 == 0 ?  "valid" : "invalid"); 
+		int checkSum = 0;
+		if (digits.length % 2 == 0) {
+			// even length checksum
+			checkSum = evenLengthChecksum(digits);
+		} else {
+			checkSum = oddLengthChecksum(digits);
+		}
+		System.out.println(checkSum % 10 == 0 ?  "valid" : "invalid");
 	}
 
 	public static void main(String[] args) {
