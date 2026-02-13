93c40473e59a81d9fa90a4fe0ba21bcd594e8b3b
Alonzo <alondiff@gmail.com>
2026-02-13 17:59:22 +0300

    re-implement count lines of code

--- CountLOC.java
+++ CountLOC.java
@@ -8,39 +8,49 @@ import java.nio.file.Paths;
 
 class CountLOC {
 
-	private static final CharSequence NEWLINE = "\n";
-	private static final CharSequence DOUBLE_SLASH = "//";
-	private static final CharSequence SINGLE_SLASH_STAR = "/*";
-	private static final CharSequence STAR_SLASH = "*/";
+	private static final String NEWLINE = "\n";
+	private static final String DOUBLE_SLASH = "//";
+	private static final String SINGLE_SLASH_STAR = "/*";
+	private static final String STAR_SLASH = "*/";
 
 	public static int count(String text) throws IOException {
+		int count = 0;
 		try (BufferedReader in = new BufferedReader(new StringReader(text))) {
-			long count = in.lines()
-				.filter(CountLOC::isNotComment)
-				.count();
-			return (int) count;
+			boolean inComment = false;
+
+			String line;
+			while ((line = in.readLine()) != null){
+				line.trim();
+				// Case where line is empty
+				if (line.isEmpty())
+					continue;
+				// Case where we encounter ending */
+				if (text.endsWith(STAR_SLASH)) {
+					if (inComment)
+						inComment = false;
+					continue;
+				}
+				// Case where we encounter //
+				if (text.startsWith(DOUBLE_SLASH)) {
+					continue;
+				}
+				//Case where we encounter starting /*
+				if (text.startsWith(SINGLE_SLASH_STAR)) {
+					inComment = true;
+					if (text.endsWith(STAR_SLASH))
+							inComment = false;
+					continue;
+				}
+
+				count++;
+			}
 		}
+		return count;
 	}
 
-	private static boolean isNotComment(String line) {
-		return !line.isEmpty() || !line.contains(NEWLINE) || !line.contains(DOUBLE_SLASH)
-			|| !line.contains(SINGLE_SLASH_STAR) || !line.contains(STAR_SLASH);
-	}
-
-	public static void main(String[] args) {
-		try {
-			/*
-			 * The reason we are chasing our tail here, is because
-			 * the interview question (for some reason) insisted on
-			 * reading the entire source file into byte String.
-			 * We could have easily used Files.lines(path)
-			 * Ofcourse we can construct path from static factory Path.get(filename)
-			 */
+	public static void main(String[] args) throws IOException {
 			byte[] readBytes = Files.readAllBytes(Paths.get(args[0]));
 			String text = new String(readBytes); 
 			System.out.println(CountLOC.count(text));
-		} catch (IOException ex) {
-			//
-		}
 	}
 }
